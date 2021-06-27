5da4f6f3eda0e941830aebe35a5d71eddda7713e
Traore Mohamed <mohamedstrore@hotmail.fr>
2021-06-27 14:50:49 +0200

    rebase

--- Command/PingCommands.js
+++ Command/PingCommands.js
@@ -1,12 +1,17 @@
-class PingCommands {
-	constructor() {
-	}
+const Discord = require('discord.js');
 
-	handle(message) {
-		message.channel.send("pong!");
-	}
+function ping(message) {
+	const neaping = new Discord.MessageEmbed()
+		.setColor('#385b55')
+		.setTitle('Mohamed test')
+		.setURL('https://github.com/Silverliee/moonbot')
+		.setAuthor('Moon Bot')
+		.setDescription('test description for moon ping commande ?')
+		.setThumbnail('https://imgur.com/a/yFEwwrd')
+		.attachFiles(['../moonbot/Image/moonBotDocumentationImg.png'])
+		.setImage('attachment://moonBotDocumentationImg.png')
+		.setFooter('test footer too')
+	message.channel.send(neaping);
 }
 
-module.exports = () => {
-	return new PingCommands();
-};
+module.exports = {ping};
